6d9802f87451f27257a5462a844b20a5aaff6c0f
Jade <jadeneye04@gmail.com>
2026-07-11 20:04:52 -0400

    removed commented out stuffs and extra row / collum

--- keyboards/jcj_test_rev_1/jcj_test_rev_1.c
+++ keyboards/jcj_test_rev_1/jcj_test_rev_1.c
@@ -1,10 +1,6 @@
 #include QMK_KEYBOARD_H
 
 bool encoder_update_kb(uint8_t index, bool clockwise){
-    // if(!encoder_update_kb(index,clockwise)){
-    //     return false;
-    // }
-
     if(clockwise){
         layer_move(1);
     }else{
